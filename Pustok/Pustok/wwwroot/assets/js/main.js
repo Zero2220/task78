4b81f40e76c68c4f7dc3e529a80d1f303f0bef14
anarpi <anarpi@code.edu.az>
2024-05-14 17:06:00 +0400

    new

---
+++ Pustok/Pustok/wwwroot/assets/js/main.js
@@ -0,0 +1,54 @@
+﻿
+
+$(document).ready(function () {
+
+    $(".book-modal").click(function (e) {
+        e.preventDefault();
+        let url = this.getAttribute("href");
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                $("#quickModal .modal-dialog").html(data)
+            })
+
+        $("#quickModal").modal('show');
+    })
+})
+
+
+document.getElementById("submitChanges").onclick = function () {
+    // Show all toast notifications
+    console.log("salam")
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+
+    // Show specific toast
+    var myAlert = document.getElementById('toastNotice');
+    var bsAlert = new bootstrap.Toast(myAlert);
+    bsAlert.show();
+};
+
+
+
+
+document.querySelectorAll(".btn-btn-primary").onclick = function () {
+    // Show all toast notifications
+    console.log("salam")
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+
+    // Show specific toast
+    var myAlert = document.getElementById('toastNotice');
+    var bsAlert = new bootstrap.Toast(myAlert);
+    bsAlert.show();
+};
+
+
+
